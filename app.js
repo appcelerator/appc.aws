8b6ca72233857e2d87df5c8a818836da6c9d0cab
vladimir-trifonov <trifonov.dev@gmail.com>
2017-04-27 18:15:53 +0300

    [ARRSOF-222] Refactoring

--- app.js
+++ app.js
@@ -2,42 +2,45 @@
  * NOTE: This file is simply for testing this connector and will not
  * be used or packaged with the actual connector when published.
  */
-var Arrow = require('arrow'),
-	server = new Arrow();
+const Arrow = require('arrow')
+const server = new Arrow()
 
 // lifecycle examples
-server.on('starting', function(){
-	server.logger.debug('server is starting!');
-});
+server.on('starting', function () {
+  server.logger.debug('server is starting!')
+})
 
-server.on('started', function(){
-	server.logger.debug('server started!');
-});
+server.on('started', function () {
+  server.logger.debug('server started!')
+})
 
 // a simple user model
-var User = Arrow.Model.extend('user',{
-	fields: {
-		name: { type: String, required: false, validator: /[a-zA-Z]{3,}/ }
-	},
-	connector: 'appc.aws.s3' // a model level connector
-});
+const User = Arrow.Model.extend('user', {
+  fields: {
+    name: { type: String, required: false, validator: /[a-zA-Z]{3,}/ }
+  },
+  connector: 'appc.aws.s3' // a model level connector
+})
 
 // create some users programmatically
 var users = [
-	{name: 'Jeff'},
-	{name: 'Nolan'},
-	{name: 'Neeraj'},
-	{name: 'Tony'},
-	{name: 'Rick'},
-	{name: 'Kranthi'}
-];
-User.create(users, function(err,users){
-	server.logger.info('Created some users',users);
-});
+  { name: 'Jeff' },
+  { name: 'Nolan' },
+  { name: 'Neeraj' },
+  { name: 'Tony' },
+  { name: 'Rick' },
+  { name: 'Kranthi' }
+]
+User.create(users, function (err, users) {
+  if (err) {
+    return server.logger.info('Error while creating some users', err)
+  }
 
+  server.logger.info('Created some users', users)
+})
 
 // add the model since we're creating outside of models directory
-server.addModel(User);
+server.addModel(User)
 
 // start the server
-server.start();
+server.start()
--- conf/example.config.js
+++ conf/example.config.js
@@ -3,13 +3,7 @@ module.exports = {
     'appc.aws.s3': {
       // AWS credentials
       accessKeyId: '',
-      secretAccessKey: '',
-      
-      // Create models based on your schema that can be used in your API.
-      generateModelsFromSchema: true,
-
-      // Whether or not to generate APIs based on the methods in generated models.
-      modelAutogen: false
+      secretAccessKey: ''
     }
   }
 }
