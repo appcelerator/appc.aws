d5b601a9a25a10d6e07fd2cd7949b805ccae9a2b
Martin Marinov <mamarinov@axway.com>
2017-12-08 11:24:02 +0200

    Update package.json

--- test/integration/S3_test.js
+++ test/integration/S3_test.js
@@ -31,7 +31,7 @@ describe('S3', function () {
     AWS.config.update(config)
     var s3 = new AWS.S3()
     s3.listBuckets(function (err) {
-      should(err).not.be.ok
+      should((err).not.be.ok)
       done()
     })
   })
@@ -39,14 +39,14 @@ describe('S3', function () {
   describe('Buckets', function () {
     it('should findAll and findOne', function (done) {
       BucketModel.findAll(function (err, result) {
-        should(err).not.be.ok
-        should(result).be.ok
+        should((err).not.be.ok)
+        should((result).be.ok)
         try {
           async.map(result, function (bucket, cb) {
-            should(bucket).be.ok
+            should((bucket).be.ok)
             BucketModel.findOne(bucket.getPrimaryKey(), function (err, bucket2) {
-              should(err).not.be.ok
-              should(bucket2).be.ok
+              should((err).not.be.ok)
+              should((bucket2).be.ok)
               should(bucket.getPrimaryKey()).be.equal(bucket2.getPrimaryKey())
               cb(null, bucket2)
             })
@@ -60,8 +60,8 @@ describe('S3', function () {
     it('should create and delete', function (done) {
       var name = 'test_bucket_' + Date.now()
       BucketModel.create({ Bucket: name }, function (err, bucket) {
-        should(err).not.be.ok
-        should(bucket).be.ok
+        should((err).not.be.ok)
+        should((bucket).be.ok)
         BucketModel.delete(bucket, done)
       })
     })
@@ -76,8 +76,8 @@ describe('S3', function () {
         acl: 'public-read'
       }
       ObjectModel.create(params, function (err, result) {
-        should(err).not.be.ok
-        should(result).be.ok
+        should((err).not.be.ok)
+        should((result).be.ok)
         result.delete(done)
       })
     })
@@ -91,16 +91,16 @@ describe('S3', function () {
         acl: 'private'
       }
       ObjectModel.create(params, function (err, result) {
-        should(err).not.be.ok
-        should(result).be.ok
+        should((err).not.be.ok)
+        should((result).be.ok)
         ObjectModel.query({ where: { bucket: 'appc-test', acl: 'private' } }, function (err, result) {
-          should(err).not.be.ok
-          should(result).be.ok
-          should(result[0].getSignedUrl({ Expires: 60 })).be.a.string
+          should((err).not.be.ok)
+          should((result).be.ok)
+          should((result[0].getSignedUrl({ Expires: 60 })).be.a.string)
           request.get(result[0].getSignedUrl({ Expires: 60 }), function (err, resp, body) {
-            should(err).not.be.ok
-            should(resp).be.an.object
-            should(body).be.a.string
+            should((err).not.be.ok)
+            should((resp).be.an.object)
+            should((body).be.a.string)
             should(body).be.equal('bar')
             result[0].delete(done)
           })
